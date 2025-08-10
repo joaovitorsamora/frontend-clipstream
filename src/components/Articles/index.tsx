8960b91a341cd9d27137567a9db4f417957fbd2a
joaovitorsamora <jvsamora00@gmail.com>
2025-08-10 17:55:48 -0300

    feat: adiciona esqueleto responsivo (Skeleton loaders) para vídeos e artigos

--- src/components/Articles/index.tsx
+++ src/components/Articles/index.tsx
@@ -1,17 +1,13 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper/modules'
 import 'swiper/css'
 import { Carousel } from '../Carousel'
 import { useFetchData } from '../../hooks/useFetchData'
 import { LinksProps } from '../../types'
-import { useMemo, useState } from 'react'
+import { useState } from 'react'
 import axios from 'axios'
-import './styles.css'
-export const Articles = () => {
-  const urls = useMemo(
-    () => ['https://backend-clipstream.vercel.app/videos', 'https://backend-clipstream.vercel.app/api/articles'],
-    []
-  )
 
+export const Articles = () => {
   const [countViews, setCountViews] = useState(0)
 
   const handleClickLink = async (url: string, id: number) => {
@@ -26,37 +22,40 @@ export const Articles = () => {
     }
   }
 
-  const { previewData } = useFetchData({ urls })
+  const { articlesData } = useFetchData()
 
   return (
-    <div className="articles">
-      <Swiper
-        breakpoints={{
-          320: { slidesPerView: 1, spaceBetween: 20 },
-          768: { slidesPerView: 2, spaceBetween: 20 },
-          1024: { slidesPerView: 3, spaceBetween: 20 },
-        }}
-      >
-        {previewData.map((item: LinksProps) => {
-          return (
-            <SwiperSlide key={item.id}>
-              <Carousel
-                id={item.id}
-                title={item.title}
-                description={item.description}
-                image={item.image}
-                onClickLink={() => handleClickLink(item.url, item.id)}
-                site={item.site}
-                url={item.url}
-                views={item.views}
-              />
-            </SwiperSlide>
-          )
-        })}
-      </Swiper>
+    <div className="w-full bg-black">
+      <div className="max-w-[1920px] mx-auto p-[4%] rounded-t-[10px] ">
+        <Swiper
+          modules={[Navigation]}
+          navigation
+          breakpoints={{
+            320: { slidesPerView: 1, spaceBetween: 20 },
+            768: { slidesPerView: 2, spaceBetween: 20 },
+            1024: { slidesPerView: 3, spaceBetween: 20 },
+            2560: { slidesPerView: 4, spaceBetween: 20 },
+          }}
+          className="relative"
+        >
+          {articlesData.map((item: LinksProps) => {
+            return (
+              <SwiperSlide key={item.id}>
+                <Carousel
+                  id={item.id}
+                  title={item.title}
+                  description={item.description}
+                  image={item.image}
+                  onClickLink={() => handleClickLink(item.url, item.id)}
+                  site={item.site}
+                  url={item.url}
+                  views={item.views}
+                />
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </div>
     </div>
   )
-}
-function setCountViews(arg0: (prev: any) => any) {
-  throw new Error('Function not implemented.')
 }
--- src/components/FilteredVideos/index.tsx
+++ src/components/FilteredVideos/index.tsx
@@ -1,30 +1,34 @@
-import { useState } from 'react'
-import { Card } from '../Card'
+import { CardComponent } from '../CardComponent'
 import { useFilteredVideos } from '../../hooks/useFilteredVideos'
 import rootReducer from '../../redux/root-reducer'
 import { useSelector } from 'react-redux'
-import './styles.css'
 
-export const FilteredVideos = () => {
+const FilteredVideos = () => {
   const { videos, searchTerms } = useSelector((state: ReturnType<typeof rootReducer>) => state.videoReducer)
-  const [loading, setLoading] = useState(true)
   const { filteredVideoList } = useFilteredVideos(videos, searchTerms)
 
+  console.log(videos)
+
   return (
-    <div>
-      {loading ? (
-        (setTimeout(() => setLoading(false), 2000), (<p>Loading...</p>))
-      ) : (
-        <section className="video-list">
-          {filteredVideoList.map((video, index) => {
-            return (
-              <div className="card" key={index}>
-                <Card id={video.id} url={video.url} title={video.title} />
-              </div>
-            )
-          })}
-        </section>
-      )}
-    </div>
+    <section
+      className="
+        grid 
+        grid-cols-1 
+        sm:grid-cols-2 
+        lg:grid-cols-3 
+        justify-items-center 
+        gap-6 
+        px-4 
+        pt-6 
+        2xl:gap-[20rem] 
+        2xl:px-[8rem]
+      "
+    >
+      {filteredVideoList.map((video) => (
+        <CardComponent key={video.id} id={video.id} url={video.url} title={video.title} />
+      ))}
+    </section>
   )
 }
+
+export default FilteredVideos
