908e0143948520221114e3f92661868b6fb753d3
joaovitorsamora <jvsamora00@gmail.com>
2025-08-10 18:06:03 -0300

    feat: adiciona suporte a dark mode com tokens via oklch

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-import { App } from './App'
-import { VideoDetail } from './pages/VideoDetail'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+
+const HomePage = lazy(() => import('./App'))
+const VideoDetailPage = lazy(() => import('./pages/VideoDetail'))
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/pages/VideoDetail/:id" element={<VideoDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <Suspense fallback={<div className="text-white text-center mt-4">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/pages/VideoDetail/:id" element={<VideoDetailPage />} />
+        </Routes>
+      </Suspense>
+    </Router>
   </Provider>
 )
