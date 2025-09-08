c83d7e6cf6457225cbce138b4ba9ec0d64858a2c
joaovitorsamora <jvsamora00@gmail.com>
2025-09-08 12:08:03 -0300

    refactor: Adjust SkeletonArticleCard layout for better responsiveness

--- src/components/SkeletonArticleCard/index.tsx
+++ src/components/SkeletonArticleCard/index.tsx
@@ -6,14 +6,16 @@ export const SkeletonArticleCard = () => {
   const { isSmallScreen, isMediumScreen, isLargeScreen } = useCheckScreen()
 
   return (
-    <div className="w-full bg-black rounded-t-[10px]">
-      <div className="max-w-[1920px] mx-auto p-[4%] bg-black overflow-x-none">
-        <div className="flex gap-4 w-full">
-          {(isSmallScreen ? [1] : isMediumScreen ? [1, 2] : isLargeScreen ? [1, 2, 3, 4] : [1, 2, 3]).map((_, i) => (
-            <Skeleton key={i} className="h-[394px] w-full rounded-xl bg-neutral-700" />
-          ))}
+    <section className="w-full">
+      <div className="w-screen -ml-[calc(50vw-50%)] -mr-[calc(50vw-50%)] bg-black py-8">
+        <div className="max-w-[1920px] mx-auto px-[4%]">
+          <div className="flex gap-4 w-full">
+            {(isSmallScreen ? [1] : isMediumScreen ? [1, 2] : isLargeScreen ? [1, 2, 3, 4] : [1, 2, 3]).map((_, i) => (
+              <Skeleton key={i} className="h-[394px] w-full rounded-xl bg-neutral-700" />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
