a1a8ceeb0de6d6a0673a088fbbe56574fc2dffdd
joaovitorsamora <jvsamora00@gmail.com>
2025-09-08 11:52:57 -0300

    feat: Add Swiper navigation and refactor article views

--- src/components/Articles/index.tsx
+++ src/components/Articles/index.tsx
@@ -1,6 +1,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation } from 'swiper/modules'
 import 'swiper/css'
+import 'swiper/css/navigation'
 import { Carousel } from '../Carousel'
 import { useFetchData } from '../../hooks/useFetchData'
 import { LinksProps } from '../../types'
@@ -8,54 +9,51 @@ import { useState } from 'react'
 import axios from 'axios'
 
 export const Articles = () => {
-  const [countViews, setCountViews] = useState(0)
+  const [countViews, setCountViews] = useState<Record<number, number>>({})
+  const { articlesData } = useFetchData()
 
   const handleClickLink = async (url: string, id: number) => {
     try {
-      const response = await axios.patch(`https://backend-clipstream.vercel.app/api/articles/${id}`, {
-        views: true,
-      })
-      setCountViews((prev: any) => ({ ...prev, [id]: response.data.views }))
+      const response = await axios.patch(`https://backend-clipstream.vercel.app/api/articles/${id}`, { views: true })
+      setCountViews((prev) => ({ ...prev, [id]: response.data.views }))
       window.open(url, '_blank')
     } catch (error) {
       console.error('Erro ao atualizar views:', error)
     }
   }
 
-  const { articlesData } = useFetchData()
-
   return (
-    <div className="w-full bg-black">
-      <div className="max-w-[1920px] mx-auto p-[4%] rounded-t-[10px] ">
-        <Swiper
-          modules={[Navigation]}
-          navigation
-          breakpoints={{
-            320: { slidesPerView: 1, spaceBetween: 20 },
-            768: { slidesPerView: 2, spaceBetween: 20 },
-            1024: { slidesPerView: 3, spaceBetween: 20 },
-            2560: { slidesPerView: 4, spaceBetween: 20 },
-          }}
-          className="relative"
-        >
-          {articlesData.map((item: LinksProps) => {
-            return (
-              <SwiperSlide key={item.id}>
+    <section className="w-full">
+      <div className="w-screen -ml-[calc(50vw-50%)] -mr-[calc(50vw-50%)] bg-black py-8">
+        <div className="max-w-[1920px] mx-auto px-[4%] rounded-t-[10px]">
+          <Swiper
+            modules={[Navigation]}
+            navigation
+            breakpoints={{
+              320: { slidesPerView: 1, spaceBetween: 20 },
+              768: { slidesPerView: 2, spaceBetween: 20 },
+              1024: { slidesPerView: 3, spaceBetween: 20 },
+              2560: { slidesPerView: 4, spaceBetween: 20 },
+            }}
+            className="relative w-full"
+          >
+            {articlesData.map((item: LinksProps) => (
+              <SwiperSlide key={item.id} className="w-auto">
                 <Carousel
                   id={item.id}
                   title={item.title}
                   description={item.description}
                   image={item.image}
-                  onClickLink={() => handleClickLink(item.url, item.id)}
+                  onClickLink={handleClickLink}
                   site={item.site}
                   url={item.url}
-                  views={item.views}
+                  views={countViews[item.id] ?? item.views}
                 />
               </SwiperSlide>
-            )
-          })}
-        </Swiper>
+            ))}
+          </Swiper>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
