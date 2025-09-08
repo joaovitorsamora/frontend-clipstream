6ea11d3af1755b31774ff3f92c7b2184ed719711
joaovitorsamora <jvsamora00@gmail.com>
2025-09-08 12:15:17 -0300

    feat: Implement protected routes and lazy loading

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,44 @@ import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import { DefaultPage } from './components/DefaultPage'
+import { ProtectedRoutes } from './components/ProtectedRoutes'
 
 const HomePage = lazy(() => import('./App'))
 const VideoDetailPage = lazy(() => import('./pages/VideoDetail'))
+
+function AppWrapper() {
+  return (
+    <Suspense fallback={<div className="text-white text-center mt-4">Loading...</div>}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <DefaultPage className="bg-gradient-to-br from-purple-950 via-black to-indigo-950">
+              <HomePage />
+            </DefaultPage>
+          }
+        />
+        <Route element={<ProtectedRoutes />}>
+          <Route
+            path="pages/VideoDetail/:id"
+            element={
+              <DefaultPage className="bg-black text-white">
+                <VideoDetailPage />
+              </DefaultPage>
+            }
+          />
+        </Route>
+      </Routes>
+    </Suspense>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <Provider store={store}>
     <Router>
-      <Suspense fallback={<div className="text-white text-center mt-4">Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/pages/VideoDetail/:id" element={<VideoDetailPage />} />
-        </Routes>
-      </Suspense>
+      <AppWrapper />
     </Router>
   </Provider>
 )
